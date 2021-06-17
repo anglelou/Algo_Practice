007f7925706f8dae16ad507e3af33382cf3594cc
Angel Soto <angells825@gmail.com>
2021-06-16 17:58:12 -0700

    basic Fibonacci seq

--- Algos/main.cpp
+++ Algos/main.cpp
@@ -2,13 +2,29 @@
 //  main.cpp
 //  Algos
 //
-//  Created by Angel Soto on 6/16/21.
+//  Created by Angel Soto.
 //
+
+// Return Nth value in Fibonacci sequence
+const int fib(int n)
+{
+    if (n <= 2)
+        return 1;
+    
+    return fib(n - 1) + fib(n - 2);
+}
 
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    std::cout << "Fib: ";
+    std::cout << fib(7) << std::endl;
+    
+    
+    
+    
     return 0;
 }
